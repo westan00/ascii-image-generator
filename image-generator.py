cd6e31fcd41a328612d3f99fca4ff02ee313c793
westan00 <samuelwestman1@gmail.com>
2025-12-02 18:14:35 +0100

    Now divides resolution by 8 and quantizes down to 10 different luminance values on greyscale

--- image-generator.py
+++ image-generator.py
@@ -1,9 +1,29 @@
 from PIL import Image
+import numpy as np
 
 
-def test():
-    im = Image.open("kolibri-os.png")
-    return im
+def downscale(img):
+    orig_width = img.width
+    orig_height = img.height
+
+    new_width = orig_width // 8
+    new_height = orig_height // 8
+
+    small_img = img.resize((new_width, new_height), resample=Image.BILINEAR)
+
+    return small_img.resize((orig_width, orig_height), resample=Image.NEAREST)
 
 
-print(test())
+def quantize_down(val):
+    return np.floor(val / 255 * 9) * (255 // 9)
+
+
+def main():
+    img = Image.open("bird.jpg")
+    img = downscale(img)
+    img = img.convert("L")
+    img = img.point(quantize_down)
+    img.show()
+
+
+main()
